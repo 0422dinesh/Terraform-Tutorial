305e043bfdd19d3aa6931406f7879e33285bfd6c
0422dinesh <88645183+0422dinesh@users.noreply.github.com>
2021-08-10 14:28:56 +0530

    ami id

--- aws-instance-first-script/variables.tf
+++ aws-instance-first-script/variables.tf
@@ -8,9 +8,9 @@ default="t2.micro"
 }
 
 variable "ami_id" {
-type = "map"
+type = map
 default = {
-ap-south-1 = "ami-04db49c0fb2215364"
+ap-south-1 = "ami-0086e63bfa49c3b49"
 eu-west-2= "ami-132b3c7efe6sdfdsfd"
 eu-central-1="ami-9787h5h6nsn"
 }
