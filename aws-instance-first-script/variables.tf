56eb2632f48b7661a13c9b3d8b2ae12c75f06e0a
0422dinesh <88645183+0422dinesh@users.noreply.github.com>
2021-08-10 14:19:11 +0530

    chenge the ami id

--- aws-instance-first-script/variables.tf
+++ aws-instance-first-script/variables.tf
@@ -1,11 +1,17 @@
+variable "access_key" { }
+variable "secret_key" { }
 variable "region" {
-  default = "ap-south-1"
+default="ap-south-1"
+}
+variable "instance_type" {
+default="t2.micro"
 }
 
-variable "ami-04db49c0fb2215364" {
-  type = "map"
-
-  default = {
-    ap-south-1    = "ami-04db49c0fb2215364"
-  }
+variable "ami_id" {
+type = "map"
+default = {
+ap-south-1 = "ami-04db49c0fb2215364"
+eu-west-2= "ami-132b3c7efe6sdfdsfd"
+eu-central-1="ami-9787h5h6nsn"
+}
 }
