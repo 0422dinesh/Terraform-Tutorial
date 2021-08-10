4ccf53ada44a5f1290e3a8a20bfd7bf2fe2b2310
0422dinesh <88645183+0422dinesh@users.noreply.github.com>
2021-08-10 14:54:20 +0530

    Update aws-instance-example.tf

--- aws-instance-first-script/aws-instance-example.tf
+++ aws-instance-first-script/aws-instance-example.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "Webserver" {
-  ami           = " ami-0086e63bfa49c3b49" 
+resource "aws_instance"  {
+  ami           = "ami-04db49c0fb2215364" 
   instance_type = "t2.micro"
 }
