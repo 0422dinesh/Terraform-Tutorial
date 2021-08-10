f0298fd26fb18378c559d86c8d837c4a4f9d7aa5
0422dinesh <88645183+0422dinesh@users.noreply.github.com>
2021-08-10 14:00:40 +0530

    update provider details

--- aws-instance-first-script/provider.tf
+++ aws-instance-first-script/provider.tf
@@ -1,4 +1,15 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "3.53.0"
+    }
+  }
+}
+
+
 provider "aws" {
-  region  = "${var.region}"
-  version = "~> 2.0"
+  region     = "ap-south-1"
+  access_key = "AKIA2IO7N7GG26YCKZ6W"
+  secret_key = "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"
 }
