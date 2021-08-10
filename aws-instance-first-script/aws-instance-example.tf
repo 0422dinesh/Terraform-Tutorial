47160db7b85e17504cc2f041054768138f141e56
0422dinesh <88645183+0422dinesh@users.noreply.github.com>
2021-08-10 13:56:12 +0530

    updated information

--- aws-instance-first-script/aws-instance-example.tf
+++ aws-instance-first-script/aws-instance-example.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "web1" {
-   ami           = "${lookup(var.ami_id, var.region)}"
-   instance_type = "t2.micro"
- }
+resource "aws_instance" "myfirstimage" {
+  ami           = "ami-04db49c0fb2215364" 
+  instance_type = "t2.micro"
+}
